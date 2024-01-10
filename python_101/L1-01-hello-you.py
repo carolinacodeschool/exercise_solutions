c020a5c81d875cbd4bfd8866437102c2c7dadafc
Sean R Reid <sean@torchcodelab.com>
2024-01-10 08:44:14 -0500

    Added Python 101 solutions

---
+++ python_101/L1-01-hello-you.py
@@ -0,0 +1,3 @@
+user_input = input('What is your name? ')
+
+print('Hello  %s' % (user_input))
---
+++ python_101/L1-02-hello-capitalize.py
@@ -0,0 +1,7 @@
+# capitalize all the things
+text = input("Give me a string, please: ")
+print(text.upper())
+
+# capitalize only the first letter, print any remaining characters
+text = input("Give me a string, please: ")
+print(text[0].upper() + text[1:])
